e39e3727c2c61b1d64f042aa06558548bb23c2e6
bruno damaso <brunodamaso@gmail.com>
2021-10-12 21:07:47 +0200

    quitar https

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace TodoApi
            
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
             //app.UseCors("CorsPolicy");
